2185a202cad8853a83eecdf616b922f0aa9c4f6f
rgraba <127319214+rgraba@users.noreply.github.com>
2023-03-22 13:58:29 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    double a = 6;
+    double b = 8;
+    double różnica = a-b;
+    double suma = a + b;
+    double iloczyn = a*b;
+    double iloraz = a/b;
+    System.out.println("Suma =" + suma + "Różnica =" + różnica + "Iloczyn =" + iloczyn + "Iloraz =" + iloraz);
+  }
+}
